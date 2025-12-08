94687b8804fbe49c97bde474a30d324196a52007
aelaina100 <aelaina100@gmail.com>
2025-12-07 21:38:14 -0500

    Explaining C++ concepts

---
+++ 20-Enumeration Homework/20-Enumeration Homework/20-Enumeration Homework.cpp
@@ -0,0 +1,59 @@
+
+/* Output the below information, exactly as it appears, utilizing Structures.
+
+ *************************************
+ Name: Ahmad El-Aina
+ Age: 99
+ City: Montréal
+ Country: Canada
+ Monthly Salary: 5000
+ Yearly Salary: 60000
+ Gender: M                          //Used to be of char data type. Now utilize the enum data type.
+ Married:1                         // Change to Marital Status, deeming it either Single or Married using enums
+ My Favourite Colour               // A new variable. Use enums
+ ************************************* 
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+enum enGender { Male, Female };
+enum enMaritalStatus { Single, Married };
+enum enFavouriteColor { White, Black, Blue = 9 };
+
+int main()
+{
+	string name, city, country;
+	unsigned short age;
+	unsigned int MonthlySalary, YearlySalary;
+
+	name = "Ahmad El-Aina";
+	age = 99;
+	city = "Montreal";
+	country = "Canada";
+	MonthlySalary = 5000;
+	YearlySalary = 60000;
+	enGender gender = enGender::Male; // stores 0 that is under the label ::Male
+	enMaritalStatus MaritalStatus = enMaritalStatus::Single; // stores 0 that is under the label ::Single
+	enFavouriteColor FavouriteColor = enFavouriteColor::Blue; // stores 9 that is under the label ::Blue
+
+	cout << "************************************* \n";
+	cout << "Name: " << name << endl;
+	cout << "Age: " << age << endl;
+	cout << "City: " << city << endl;
+	cout << "Country: " << country << endl;
+	cout << "Monthly Salary: " << MonthlySalary << endl;
+	cout << "Yearly Salary: " << YearlySalary << endl;
+	cout << "Gender [Male(0) & Female(1)]: " << gender << endl; //0
+	cout << "Marital Status[Single(0) & Married(1)]: " << MaritalStatus << endl;    //0
+	cout << "My Favourite Color: " << FavouriteColor << endl; // 9
+	cout << "************************************* \n";
+
+
+	return 0;
+}
+//// brainstorming: Is it possible to couple the enumeration concept with user input ? (Will be explained in future lectures).
+ 
+
